53cf2aa645166c6a39bbd4ae1c213ada607dabbb
Christoph Hellwig <hch@lst.de>
2021-01-28 19:14:14 +0100

    kallsyms: only build {,module_}kallsyms_on_each_symbol when required
    kallsyms_on_each_symbol and module_kallsyms_on_each_symbol are only used
    by the livepatching code, so don't build them if livepatching is not
    enabled.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- kernel/module.c
+++ kernel/module.c
@@ -4493,6 +4493,7 @@ unsigned long module_kallsyms_lookup_name(const char *name)
 	return ret;
 }
 
+#ifdef CONFIG_LIVEPATCH
 int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 					     struct module *, unsigned long),
 				   void *data)
@@ -4523,6 +4524,7 @@ int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 	mutex_unlock(&module_mutex);
 	return ret;
 }
+#endif /* CONFIG_LIVEPATCH */
 #endif /* CONFIG_KALLSYMS */
 
 /* Maximum number of characters written by module_flags() */
